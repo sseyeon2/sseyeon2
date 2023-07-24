d8a1f81d049281f2f789fb93450016455df0c6dc
sseyeon2 <sseyoun2@gmail.com>
2023-07-24 12:39:14 +0900

    soldac

--- soldac-basic/soldoc/js/custom.js
+++ soldac-basic/soldoc/js/custom.js
@@ -1,39 +1,18 @@
 $(document).ready(function(e) {
-	$('.main_slide_wrap .main_slide').slick({
+	$('.sup-cont .slide-box-wrap ul').slick({
 		infinite: true,
 		speed: 300,
 		fade: true,
-		arrows: true,
+		arrows: false,
 		autoplay: true,
 		pauseOnHover : false, //마우스 호버시슬라이드 멈추는거
 		autoplaySpeed: 3000,
 		cssEase: 'linear',
-		prevArrow: $('.arrow_wrap .prev'),
-		nextArrow: $('.arrow_wrap .next'),
-		dotsClass: 'dots_custom',
+		dotClass: 'btn-dot',
 		dots: true
 	});
 
-	$('.play').click(function(){
-		$('.main_slide_wrap .main_slide').slick('slickPlay');
-	});
-	 
-	$('.stop').click(function(){
-		$('.main_slide_wrap .main_slide').slick('slickPause');
-	});
 	
-
-	$('.sub2_slide .slide').slick({
-		infinite: true,
-		speed: 300,
-		fade: true,
-		arrows: true,
-		autoplay: true,
-		pauseOnHover : false,
-		autoplaySpeed: 3000,
-		cssEase: 'linear',
-		dots: true
-	});
 	// $('.main_section .slide_banner').on('beforeChange', function(event, slick, currentSlide, nextSlide){
 	// 	if( nextSlide ){
 	// 		$('.main_section .slide_banner .slide_dot .circle').removeClass("active");
